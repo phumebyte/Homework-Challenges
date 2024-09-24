96e7aea3bae1c89f2eddb36699475a46626b4545
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-24 16:27:13 +0200

    Editted if-else statement to debug balance calculation

--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -3,15 +3,19 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if ((hourOfDay !== null) && (minuteOfDay !== null) && (hourOfDay == 0) && (minuteOfDay == 0)) {
+	const taxAsDecimal = parseInt(tax) / 100;
+  	const startingAfterTax = salary - (salary * taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent
+	console.log(balance.toFixed(2))
+} else {
+	console.log("Balance could not be calculated");
+
 }
 	
-console.log(balance.toFixed(3))
+
