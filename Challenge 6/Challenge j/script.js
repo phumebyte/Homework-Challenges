b653182cc011d5d04f4d0fd8422f8787c891d8d5
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-24 09:19:03 +0200

    Debugged and explained Challenge J

--- Challenge 6/Challenge j/script.js
+++ Challenge 6/Challenge j/script.js
@@ -1,2 +1,5 @@
-const value = "3"
+const value = 3
 console.log(value + 4 + value)
+
+// 343 was logged initially due to string concantenation that happens when you add strings, it treats the whole string as a single value
+// including the number data type, which is also concantenated to the string
