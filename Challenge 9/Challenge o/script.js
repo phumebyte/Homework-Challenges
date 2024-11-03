9ed127d1b075e5f9ca98da27795e6e447a4faa6b
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-03 22:43:11 +0200

    Wrapped date accesses with new Date() and fixed random holiday selection

--- Challenge 9/Challenge o/script.js
+++ Challenge 9/Challenge o/script.js
@@ -74,27 +74,27 @@ console.log('Name change:', holidays[christmas].name !== copiedChristmas.name &&
 console.log('Date change:', holidays[christmas].date !== correctDate && correctDate) // come back
 
 const firstHolidayTimestamp = Math.min(
-    holidays[0].date.getTime(),
-    holidays[1].date.getTime(),
-    holidays[2].date.getTime(),
-    holidays[3].date.getTime(),
-    holidays[4].date.getTime(),
-    holidays[5].date.getTime(),
-    holidays[6].date.getTime(),
-    holidays[7].date.getTime(),
-    holidays[8].date.getTime(),
+    new Date(holidays[0].date).getTime(),
+    new Date(holidays[1].date).getTime(),
+    new Date(holidays[2].date).getTime(),
+    new Date(holidays[3].date).getTime(),
+    new Date(holidays[4].date).getTime(),
+    new Date(holidays[5].date).getTime(),
+    new Date(holidays[6].date).getTime(),
+    new Date(holidays[7].date).getTime(),
+    new Date(holidays[8].date).getTime(),
 )
 
 const lastHolidayTimestamp = Math.max(
-    holidays[0].date.getTime(),
-    holidays[1].date.getTime(),
-    holidays[2].date.getTime(),
-    holidays[3].date.getTime(),
-    holidays[4].date.getTime(),
-    holidays[5].date.getTime(),
-    holidays[6].date.getTime(),
-    holidays[7].date.getTime(),
-    holidays[8].date.getTime(),
+    new Date(holidays[0].date).getTime(),
+    new Date(holidays[1].date).getTime(),
+    new Date(holidays[2].date).getTime(),
+    new Date(holidays[3].date).getTime(),
+    new Date(holidays[4].date).getTime(),
+    new Date(holidays[5].date).getTime(),
+    new Date(holidays[6].date).getTime(),
+    new Date(holidays[7].date).getTime(),
+    new Date(holidays[8].date).getTime(),
 )
 
 const firstHoliday = new Date (firstHolidayTimestamp)
@@ -107,7 +107,7 @@ const lastMonth = lastHoliday.getMonth()
 console.log(`${firstDay}/${firstMonth}/${currentYear}`)
 console.log(`${lastDay}/${lastMonth}/${currentYear}`)
 
-const randomHoliday = holidays[Math.random].date
-const randomDay = randomHoliday.getDate()
-const randomMonth = randomHoliday.getMonth()
+const randomHoliday = holidays[Math.floor(Math.random() * 9)];
+const randomDay = new Date(randomHoliday.date).getDay()
+const randomMonth = new Date(randomHoliday.date).getMonth()
 console.log(`${randomDay}/${randomMonth}/${currentYear}`)
